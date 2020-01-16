53bb18f83f65e347a62d2a1403334cd023bc42c6
DFSWeb <contato@dfsweb.com.br>
2020-01-16 16:58:04 -0300

    Criando um módulo e includindo

---
+++ matematica.js
@@ -0,0 +1,16 @@
+var Matematica = {
+		somar: function(x,y){
+			return x+y;
+		},
+		subtrair: function(x,y){
+			return x-y;
+		},
+		multiplicar: function(x,y){
+			return x*y;
+		},
+		dividir: function(x,y){
+			return x/y;
+		}
+};
+
+module.exports = Matematica;
--- teste.js
+++ teste.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
+var mat = require('./matematica');
 
-app.use(express.static(__dirname+'/www'));
-
-app.listen(8080);
+console.log("A soma de 1+2 = "+mat.somar(1,2));
+console.log("A subtração de 20 - 6 = "+mat.subtrair(20,6));
+console.log("A multiplicação de 3 x 5 = "+mat.multiplicar(3,5));
+console.log("A divisão de 100/4 = "+mat.dividir(100,4));
