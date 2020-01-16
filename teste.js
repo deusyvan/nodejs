982260d9cf3cdc17d67caf4468fd42960b315924
DFSWeb <contato@dfsweb.com.br>
2020-01-16 15:43:58 -0300

    Manipulando arquivos

--- teste.js
+++ teste.js
@@ -1,2 +1,17 @@
-var nome = 'deusyvan';
-console.log("Teste funcionando!"+nome);
+var fs = require('fs');
+
+fs.readFile('texto.txt', function(e,r){
+	var log = r.toString();//Transforma em string
+	
+	var linhas = log.split('\n');//Cria um array de linhas
+	
+	for ( var i in linhas) {
+		var partes = linhas[i].split(' ');
+		
+		console.log("-------------------");
+		console.log('DATA: '+partes[0]);
+		console.log('TIPO: '+partes[1]);
+		console.log('MSG: '+partes[2]);
+	}
+	
+})
